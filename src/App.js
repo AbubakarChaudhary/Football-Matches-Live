7cbd0a552d8a19edb703c046a457b11768e01c9b
Your Name <mahboobabubakar786@gmail.com>
2023-07-06 17:36:44 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import LiveScore from './LiveScore/LiveScore';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='wraper'>
+        <h1>Live Football Score</h1>
+        <LiveScore />
+      </div>
+    </div>
+  );
+}
+
+export default App;
